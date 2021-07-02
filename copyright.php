7f0b8eb9aba9917ea17be01e418b78d31903aed2
Manfred Rosenboom <maroph@gmail.com>
2021-07-02 15:05:25 +0200

    fix some smaller bugs

--- copyright.php
+++ copyright.php
@@ -4,7 +4,6 @@
 <title>Copyright</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta charset="utf-8"/>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <meta name="author" content="Manfred Rosenboom"/>
 <meta name="description" content="site copyright information"/>
 <meta name="robots" content="index, follow"/>
@@ -17,25 +16,16 @@
 
 <section>
 <p>
-<a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/deed.en"><img alt="Creative Commons License" title="Creative Commons License" style="border-width:0;" src="http://i.creativecommons.org/l/by/3.0/de/88x31.png" /></a><br/><span>The data on this web site</span> by <a href="http://m.marochess.de/">Manfred Rosenboom</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/deed.en">Creative Commons Attribution 3.0 Germany License</a>.
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Namensnennung 4.0 International Lizenz</a>.
 </p>
-<p>Copyright (under above license) 1996 - 2013 Manfred Rosenboom</p>
-<p>
-You may distribute the works from this site as long as my name and
-this copyright notice are included with each piece.
-</p>
-<p> Please link my work back to this site </p>
-<ul>
-<li><a href="http://m.marochess.de/">http://m.marochess.de/</a></li>
-</ul>
+<p>Copyright (under above license) 1996 - 2021 Manfred Rosenboom</p>
 </section>
 
 <footer>
 <p>
 <span class="noprint nomobile">Validate <a href="http://validator.w3.org/check/referer" target="_blank">HTML</a> , Validate <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS</a><br/></span>
 Last updated: <?php echo(date("r", filemtime($_SERVER['SCRIPT_FILENAME']))); ?><br/>
-Manfred Rosenboom (<a href="mailto:marochess@yahoo.com">marochess@yahoo.com</a>)<br/>
-<a href="http://about.me/maroph" target="_blank">About Me</a>
+<a href="https://manfred.rosenboom.name/" target="_blank">Manfred Rosenboom</a>
 </p>
 </footer>
 </body>
--- infosystems.php
+++ infosystems.php
@@ -4,7 +4,6 @@
 <title>Infosystems</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta charset="utf-8"/>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <meta name="author" content="Manfred Rosenboom"/>
 <meta name="description" content=""/>
 <meta name="keywords" content="HTML5"/>
@@ -41,7 +40,7 @@
 
 <footer>
 <p>
-<span class="noprint nomobile">Validate <a href="http://validator.w3.org/check/referer" target="_blank">HTML</a> , Validate <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS</a><br/></span>
+<span class="noprint nomobile">Validate <a href="https://validator.w3.org/check/referer" target="_blank">HTML</a> , Validate <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS</a><br/></span>
 Last updated: <?php echo(date("r", filemtime($_SERVER['SCRIPT_FILENAME']))); ?><br/>
 <a href="./copyright.php">Copyright</a> Manfred Rosenboom<br/>
 </p>
